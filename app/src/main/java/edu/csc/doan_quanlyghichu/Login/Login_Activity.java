80351c8920efe37b7ba6ba5e56ad549f6206cdd4
DungVu0302 <vuvansin1999@gmail.com>
2020-12-30 23:14:41 +0700

    Day 1

---
+++ app/src/main/java/edu/csc/doan_quanlyghichu/Login/Login_Activity.java
@@ -0,0 +1,16 @@
+package edu.csc.doan_quanlyghichu.Login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.csc.doan_quanlyghichu.R;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+    }
+}
